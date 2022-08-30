108ca086ef8e85c50537898350a92cd60513b86c
alican yilmaz <alicanyilmaz101@gmail.com>
2022-08-30 16:25:05 +0300

    Initial Commit

---
+++ BASIC_MVVM_EXAMPLE/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BASIC_MVVM_EXAMPLE
+//
+//  Created by Alican Yilmaz on 30.08.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
